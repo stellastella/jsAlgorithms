2c445de8caf85f408232aa4fae50b608ac771755
stellastella <iwuchukwustella333@gmail.com>
2023-11-24 17:05:12 -0600

    inital commit

---
+++ jsBaby/celsiusToFahrenheit.js
@@ -0,0 +1,16 @@
+// This function converts celsius to fahrenheit
+
+function CtoF(celsius){
+    let fahrenheit;
+    fahrenheit = celsius * (9/5) + 32  +"Degree Fahernheit";
+    return fahrenheit;
+}
+
+
+// here calling the function and passing a celsius value would give a resulting fahrenheit value.
+/*   
+CtoF(10);
+
+Result = 10degrees Fahrenheit
+
+*/
